0a7a3400ab446adc632ab414cb8f6879d25083fd
Isaac Brist <isaacbrist@gmail.com>
2022-07-15 21:22:10 -0500

    I set up the files and added too many things for one commit. I mostly finished the Feeling component, I set up the barebones for the dom of all of the components, and I set up a reducer.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Array of products, this reducer is complete
+const answerData = (state = [], action) => {
+  if (action.type === 'ADD_NEW_ANSWER') {
+    return [...state, action.payload];
+  }
+  return state;
+};
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(combineReducers({answerData}), applyMiddleware(logger));
+// Wrap our App in a Provider, this makes Redux available in
+// our entire application
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
